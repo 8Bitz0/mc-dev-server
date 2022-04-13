4afd9d58f90ffec02237f7cd388c7cf01437a831
BuildTools <unconfigured@null.spigotmc.org>
2022-04-13 09:28:24 -0400

    Add base config

--- classes/Config.py
+++ classes/Config.py
@@ -1,1 +1,15 @@
 debug = True
+
+# The directory of the JDK folder
+jdkDir = ""
+
+# Additional startup flags
+extraFlags = ""
+
+# Minimum allocated RAM
+# Counted in megabytes
+minMem = "512"
+
+# Maximum allocated RAM
+# Counted in megabytes
+maxMem = "4096"
--- classes/User.py
+++ classes/User.py
@@ -1,3 +1,6 @@
 from . import Logging
 from . import Generator
 
+Logging.info("Modules imported!")
+Logging.info("Reading config...")
+
